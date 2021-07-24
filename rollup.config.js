65982df5e5e5e7534b095e393497538e3f22832c
Adrijan <adrijan9@gmail.com>
2021-07-24 10:44:04 +0200

    added utils

--- rollup.config.js
+++ rollup.config.js
@@ -11,9 +11,27 @@ export default [
         external: [ 'moment' ],
     },
     {
+        input: './src/utils.ts',
+        output: {
+            file: './lib/utils.esm.js',
+            format: 'esm',
+        },
+        plugins: [typescript()],
+        external: [ 'moment' ],
+    },
+    {
         input: './src/index.ts',
         output: {
             file: './lib/index.js',
+            format: 'cjs',
+        },
+        plugins: [typescript()],
+        external: [ 'moment' ],
+    },
+    {
+        input: './src/utils.ts',
+        output: {
+            file: './lib/utils.js',
             format: 'cjs',
         },
         plugins: [typescript()],
---
+++ src/utils.ts
@@ -0,0 +1,47 @@
+import ArrayHasItems from './Utils/Array/ArrayHasItems';
+import GetDuplicatesFromArray from './Utils/Array/GetDuplicatesFromArray';
+import IsArray from './Utils/Array/IsArray';
+import IsBoolean from './Utils/Boolean/IsBoolean';
+import IsFalsy from './Utils/Boolean/IsFalsy';
+import IsTruthy from './Utils/Boolean/IsTruthy';
+import DateFormats from './Utils/Dates/DateFormats';
+import IsDate from './Utils/Dates/IsDate';
+import Clone from './Utils/Json/Clone';
+import Parse from './Utils/Json/Parse';
+import Stringify from './Utils/Json/Stringify';
+import IsNumber from './Utils/Numbers/IsNumber';
+import IsObject from './Utils/Object/IsObject';
+import IsObjectEmpty from './Utils/Object/IsObjectEmpty';
+import Isset from './Utils/Object/Isset';
+import ObjectValues from './Utils/Object/ObjectValues';
+import IsEmptyString from './Utils/String/IsEmptyString';
+import IsString from './Utils/String/IsString';
+import UcFirst from './Utils/String/UcFirst';
+import IsFunction from './Utils/IsFunction';
+import IsNull from './Utils/IsNull';
+import IsUndefined from './Utils/IsUndefined';
+
+export {
+    ArrayHasItems,
+    GetDuplicatesFromArray,
+    IsArray,
+    IsBoolean,
+    IsFalsy,
+    IsTruthy,
+    DateFormats,
+    IsDate,
+    Clone,
+    Parse,
+    Stringify,
+    IsNumber,
+    IsObject,
+    IsObjectEmpty,
+    Isset,
+    ObjectValues,
+    IsEmptyString,
+    IsString,
+    UcFirst,
+    IsFunction,
+    IsNull,
+    IsUndefined,
+};
